245f880676eabb3550f92cb905b1a4e35fe32903
nativeai <98702735+nativeai@users.noreply.github.com>
2022-07-29 22:52:07 -0400

    Added overview & Team component - routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KpiComponent } from './modules/kpi/kpi.component';
+import { OverviewComponent } from './modules/overview/overview.component';
+import { TeamComponent } from './modules/team/team.component';
 
 const routes: Routes = [
   
@@ -8,6 +10,16 @@ const routes: Routes = [
     path: 'kpi',
     component: KpiComponent,
     loadChildren: () => import('./modules/kpi/kpi.module').then(m => m.KpiModule)
+  },
+  {
+    path: 'team',
+    component: TeamComponent,
+    loadChildren: () => import('./modules/team/team.module').then(m => m.TeamModule)
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent,
+    loadChildren: () => import('./modules/overview/overview.module').then(m => m.OverviewModule)
   },
   {
     path: '**', 
--- src/app/modules/kpi/kpi-routing.module.ts
+++ src/app/modules/kpi/kpi-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamComponent } from '../team/team.component';
 import { KpiComponent } from './kpi.component';
 
 const routes: Routes = [
   {
     path: '',
     component: KpiComponent
-  }];
+  },
+  // {
+  //   path: '/analysis',
+  //   component: kpiComponent
+  // },
+  // test note function 
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
